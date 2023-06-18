d35711fac27e7acb76e5149756621fca69816fe6
Amit Kumar Singh <62261219+amitsingh139@users.noreply.github.com>
2023-06-19 00:03:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const channelList = document.getElementById('channel-list');
   const player = document.getElementById('player');
-  const githubRawURL = 'Tata Play.m3u';
+  const githubRawURL = 'JIO TV2023.m3u';
 
   fetch(githubRawURL)
     .then(response => response.text())
